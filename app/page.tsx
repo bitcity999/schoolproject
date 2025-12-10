c7034fa09a9f87fa762dac61a79b838ea97e5691
Prakhar Vohra <webfirex@gmail.com>
2025-12-10 21:37:35 +0530

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
 
       {!blockIframe && showIframe && (
         <iframe
-          src="https://phpstack-1561662-6062061.cloudwaysapps.com"
+          src="https://phpstack-1561662-6062061.cloudwaysapps.com/index.html"
           className="w-full h-full"
         />
       )}
